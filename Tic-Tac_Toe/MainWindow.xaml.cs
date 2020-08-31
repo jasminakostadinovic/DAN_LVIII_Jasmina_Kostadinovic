c050d9d8d67998e669adcc63c70c91bab38bcf6e
jasmi <jasmi@DESKTOP-D1R4JNJ>
2020-08-31 11:13:34 +0200

    tic tac toe logic and UI

--- Tic-Tac_Toe/MainWindow.xaml.cs
+++ Tic-Tac_Toe/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Tic_Tac_Toe
 {
@@ -23,6 +13,104 @@ namespace Tic_Tac_Toe
         public MainWindow()
         {
             InitializeComponent();
+            ticTac = new TicTac();
+
+            this.newGame();
         }
+
+        #region privateMembers
+
+        /// <summary>
+        /// instance of <see cref="TicTac"/>
+        /// </summary>
+        private TicTac ticTac;
+        /// <summary>
+        /// initialise new game
+        /// </summary> 
+
+        private void newGame()
+        {
+            ticTac.defaultTileInit();
+
+            Container.Children.Cast<Button>().ToList().ForEach(button =>
+            {
+                button.Content = string.Empty;
+                button.Background = Brushes.White;
+                button.Foreground = Brushes.Blue;
+            });
+        }
+
+        /// <summary>
+        /// button click Event Handler....
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// 
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+
+            var column = Grid.GetColumn(button);
+            var row = Grid.GetRow(button);
+
+            var gridIndex = column + (row * 3);
+
+            if (ticTac.PlayerState && ticTac.GameState)
+            {
+                if (ticTac.tileValues[gridIndex] != TicTac.BoxState.free)
+                {
+                    MessageBox.Show("Already Filled!");
+                }
+
+                else
+                {
+                    ticTac.tileValues[gridIndex] = TicTac.BoxState.cross;
+                    button.Content = 'X';
+                    ticTac.PlayerState = false;
+                }
+                ticTac.getWinner(TicTac.BoxState.cross);
+            }
+
+
+            if (!ticTac.PlayerState && ticTac.GameState)
+            {
+                var btn = Container.Children.Cast<Button>().ToArray();
+                var index = ticTac.computerPlay();
+                btn[index].Content = 'O';
+                btn[index].Foreground = Brushes.Red;
+                ticTac.tileValues[index] = TicTac.BoxState.zero;
+                ticTac.PlayerState = true;
+                ticTac.getWinner(TicTac.BoxState.zero);
+            }
+
+            if (ticTac.GameState != true)
+            {
+                if (ticTac.winner == TicTac.IdentifyWinner.stalemate)
+                {
+                    Container.Children.Cast<Button>().ToList().ForEach(btn=>
+                    {
+                        button.Background = Brushes.Gold;
+                        button.Foreground = Brushes.Black;
+                    });
+
+                    MessageBox.Show($"{ticTac.winner.ToString()}");
+                    this.newGame();
+                }
+
+                if (ticTac.winner == TicTac.IdentifyWinner.player || ticTac.winner == TicTac.IdentifyWinner.computer)
+                {
+                    var btn = Container.Children.Cast<Button>().ToArray();
+
+                    for (int i = 0; i < ticTac.maxRowSize; ++i)
+                    {
+                        btn[ticTac.winSegments[i]].Background = Brushes.Green;
+                    }
+                    MessageBox.Show($"{ticTac.winner.ToString()} wins GameOver");
+                    this.newGame();
+                }
+            }
+        }
+
+        #endregion
     }
 }
---
+++ Tic-Tac_Toe/TicTac.cs
@@ -0,0 +1,388 @@
+﻿using System;
+
+namespace Tic_Tac_Toe
+{
+    class TicTac
+    {
+        #region privateMembers
+        /// <summary>
+        /// maxsize of tilevales array <see cref="tileValues"/>
+        /// </summary>
+        private int maxSize { get; set; } = 9;
+
+        private bool foundPattern { get; set; }
+        #endregion
+
+
+        #region publicMembers
+
+        #region publicVariables
+
+        public short maxRowSize { get; private set; } = 3;
+        public short maxcolSize { get; private set; } = 3;
+
+        public bool PlayerState { get; set; }
+
+        public bool GameState { get; private set; }
+
+        public short[] winSegments;
+
+        public IdentifyWinner winner;
+
+        #endregion
+
+        /// <summary>
+        /// state of gridBox
+        /// </summary>
+        public enum BoxState
+        {
+            /// empty game tile
+            free,
+            /// game tile with X
+            cross,
+            /// game tile with O
+            zero
+        }
+
+        public enum IdentifyWinner
+        {
+            NULL,
+            player,
+            computer,
+            stalemate
+        }
+
+        public BoxState[] tileValues;
+
+        public TicTac()
+        {
+            this.winSegments = new short[3] { 0, 0, 0 };
+            this.tileValues = new BoxState[9];
+            this.winner = new IdentifyWinner();
+            this.foundPattern = false;
+            this.PlayerState = true;
+            this.GameState = true;
+        }
+
+        public void defaultTileInit()
+        {
+            for (int i = 0; i < maxSize; ++i)
+            {
+                tileValues[i] = BoxState.free;
+            }
+
+            winner = IdentifyWinner.NULL;
+            this.foundPattern = false;
+            this.PlayerState = true;
+            this.GameState = true;
+        }
+
+
+        #region ComputerTurn
+
+        public int computerPlay()
+        {
+
+            int index = 0;
+
+            short? val = processAI();
+
+            if (val != null)
+            {
+                index = (int)val;
+                return index;
+            }
+
+
+            while (true)
+            {
+                Random random = new Random();
+                index = random.Next(0, 8);
+
+                if (tileValues[index] == BoxState.free)
+                    break;
+            }
+
+            return index;
+        }
+
+        #endregion
+
+        public void checkGameState()
+        {
+            if (this.winner == IdentifyWinner.NULL)
+            {
+                foreach (var tile in this.tileValues)
+                {
+                    if (tile == TicTac.BoxState.free)
+                    {
+                        this.GameState = true;
+                        break;
+                    }
+                    else
+                    {
+                        this.GameState = false;
+                    }
+
+                }
+
+                if (this.GameState != true)
+                {
+                    winner = IdentifyWinner.stalemate;
+                }
+
+            }
+        }
+
+
+        public void getWinner(BoxState tempstate)
+        {
+            if (this.GameState != false)
+            {
+                getwin(tempstate);
+
+                if (foundPattern)
+                {
+                    if (tempstate == BoxState.cross)
+                        winner = IdentifyWinner.player;
+
+                    else if (tempstate == BoxState.zero)
+                        winner = IdentifyWinner.computer;
+                }
+
+                this.checkGameState();
+            }
+
+        }
+
+        private void getwin(BoxState boxState)
+        {
+
+            #region Horizontalcheck
+
+            short segindex = 0;
+            short temp = this.maxRowSize;
+            short temp2 = 0;
+            for (short j = 0; j < this.maxRowSize; ++j)
+            {
+                for (short i = temp2; i < temp; ++i)
+                {
+                    if (tileValues[i] != boxState)
+                    {
+                        this.GameState = true;
+                        foundPattern = false;
+                        break;
+                    }
+
+                    else if (tileValues[i] == boxState)
+                    {
+                        this.winSegments[segindex++] = i;
+                        this.GameState = false;
+                        foundPattern = true;
+                    }
+                }
+
+                if (foundPattern)
+                    break;
+
+                segindex = 0;
+                temp += this.maxRowSize;
+                temp2 += 2 + 1;
+            }
+
+            #endregion
+
+            if (foundPattern) return; ///
+
+
+            #region VerticalCheck
+            segindex = 0;
+            temp = 6;
+            temp2 = 0;
+            short temp3 = temp2;
+
+            for (short j = 0; j < this.maxcolSize; ++j)
+            {
+                for (short i = temp2; i <= temp; i += 3)
+                {
+                    if (tileValues[i] != boxState)
+                    {
+                        this.GameState = true;
+                        foundPattern = false;
+                        break;
+                    }
+
+                    else if (tileValues[i] == boxState)
+                    {
+                        this.winSegments[segindex++] = i;
+                        this.GameState = false;
+                        foundPattern = true;
+                    }
+                }
+
+                if (foundPattern)
+                    break;
+
+                segindex = 0;
+                temp += 1;
+                temp3 += 1;
+                temp2 = temp3;
+            }
+
+            #endregion
+
+            if (foundPattern) return; ///
+
+
+            #region diagonalCheck
+
+            segindex = 0;
+            temp = 8;
+            temp2 = 0;
+            short incr = 4;
+
+            for (short j = 0; j < this.maxcolSize - 1; ++j)
+            {
+                for (short i = temp2; i <= temp; i += incr)
+                {
+                    if (tileValues[i] != boxState)
+                    {
+                        this.GameState = true;
+                        foundPattern = false;
+                        break;
+                    }
+
+                    else if (tileValues[i] == boxState)
+                    {
+                        this.winSegments[segindex++] = i;
+                        this.GameState = false;
+                        foundPattern = true;
+                    }
+                }
+
+                if (foundPattern)
+                    break;
+
+                segindex = 0;
+                temp -= 2;
+                temp2 += 2;
+                incr -= 2;
+            }
+            #endregion
+        }
+
+        private short? processAI()
+        {
+
+            short count = 0;
+            short? Aival = null;
+
+            #region Horizontalcheck
+
+            short temp = this.maxRowSize;
+            short temp2 = 0;
+
+            for (short j = 0; j < this.maxRowSize; ++j)
+            {
+                for (short i = temp2; i < temp; ++i)
+                {
+
+                    if (tileValues[i] == BoxState.cross)
+                    {
+                        ++count;
+                    }
+
+                    else
+                    {
+                        Aival = i;
+                    }
+                }
+
+                if (count == 2 && tileValues[(int)Aival] != BoxState.zero)
+                    break;
+
+                count = 0;
+                temp += this.maxRowSize;
+                temp2 += 2 + 1;
+            }
+
+            if (count == 2 && Aival != null) return (short)Aival;
+
+            #endregion
+
+            #region VerticalCheck
+            count = 0;
+            temp = 6;
+            temp2 = 0;
+            short temp3 = temp2;
+
+            for (short j = 0; j < this.maxcolSize; ++j)
+            {
+                for (short i = temp2; i <= temp; i += 3)
+                {
+
+                    if (tileValues[i] == BoxState.cross)
+                    {
+                        ++count;
+                    }
+
+                    else
+                    {
+                        Aival = i;
+                    }
+                }
+
+
+                if (count == 2)
+                    break;
+
+                count = 0;
+                temp += 1;
+                temp3 += 1;
+                temp2 = temp3;
+            }
+
+            if (count == 2 && Aival != null) return (short)Aival;
+
+            #endregion
+
+            #region diagonalCheck
+
+            count = 0;
+            temp = 8;
+            temp2 = 0;
+            short incr = 4;
+
+            for (short j = 0; j < this.maxcolSize - 1; ++j)
+            {
+                for (short i = temp2; i <= temp; i += incr)
+                {
+                    if (tileValues[i] == BoxState.cross)
+                    {
+                        ++count;
+                    }
+
+                    else
+                    {
+                        Aival = i;
+                    }
+                }
+
+                if (count == 2)
+                    break;
+
+                count = 0;
+                temp -= 2;
+                temp2 += 2;
+                incr -= 2;
+            }
+
+            if (count == 2 && Aival != null) return (short)Aival;
+            #endregion
+
+            return null;
+        }
+
+        #endregion
+    }
+}
